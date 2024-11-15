b9b7829f25f0207e58223a6876df58f5b83c5c72
Your Name <reithankurau@gmail.com>
2024-11-15 12:50:57 +0900

    Some changes (background)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.active_storage.service = :cloudinary
 
   # Enable server timing
   config.server_timing = true
